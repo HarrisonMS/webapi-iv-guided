89ff7abe06a23aee7ff741eafeebdf7aa2ee3a4d
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-12-05 10:08:06 -0800

    dynamic deploy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./api/server.js');
-
+const port = process.env.PORT || 1112;
 server.listen(1112, () => {
-  console.log('\n*** Server Running on http://localhost:1112 ***\n');
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
